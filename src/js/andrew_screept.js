349cf789d689817da647002da1b40e500d69a1dc
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-02-03 02:27:23 +0200

    andrew_sript rewrite

--- src/js/andrew_screept.js
+++ src/js/andrew_screept.js
@@ -1,89 +1,88 @@
-        // <button id='main_btn'>Trending</button>
-        // <input id='search_movie' type="text" name="input">
-        // <button id='search_btn'>Search</button>
-
-console.log(' -  * test * - ');
-const API_KEY = '50540b41e66ef631d8d57e13679f9024';
+const API_KEY = '63240915768e2fa639cf91287e69284e';
 const TRENDING_TIME = 'week';
 
-let mainBtn = document.getElementById('main_btn');
-mainBtn.addEventListener('click', Trending);
+Trending();
 
-let searchBtn = document.getElementById('search_btn');
-searchBtn.addEventListener('click', Searh);
-let inputMuvie = document.getElementById('search_movie');
+const formEl = document.querySelector('.search-form');
+const message = document.querySelector('.message');
+const markupContainer = document.querySelector('.movie-markup');
 
+formEl.addEventListener('submit', Search);
 
-function Searh() {
+function Search(evt) {
+  evt.preventDefault();
+  let page = 1;
+  const inputValue = formEl.elements.searchQuery.value;
+  console.log(formEl.elements);
+  if (!inputValue) {
+    message.textContent =
+      'Search result not successful. Enter the correct movie name and';
+    return;
+  }
 
-    // console.log(inputMuvie.value);
-    if (!inputMuvie.value) {
-        console.log('no input');
-        return;
-    }
+  const fetchMovies = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${inputValue}&page=${page}`
+    );
+    const movies = await response.json();
+    return movies;
+  };
 
-    console.log('Search');
-
-    const fetchMovies = async () => {
-        const response = await fetch('https://api.themoviedb.org/3/search/movie?api_key=' +
-            API_KEY + '&language=en-US&query=' + inputMuvie.value + '&page=1&include_adult=false');
-        const movies = await response.json();
-        return movies;
-    };
-
-    fetchMovies()
-        .then(movies => {
-            console.log('serch f', movies.results);
-            localStorage.setItem('currentPageMuvie', JSON.stringify(movies.results));
+  fetchMovies()
+    .then(movies => {
+      Render(movies.results);
+      localStorage.setItem(
+        'currentPage',
+        JSON.stringify({
+          type: 'serched',
+          result: movies,
         })
-        .catch(error => console.log(error));
-
-
-
-    Render(localStorage.getItem('currentPageMuvie'));
-
+      );
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 function Trending() {
-    console.log('Trending');
+  const fetchTrending = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/trending/movie/${TRENDING_TIME}?api_key=${API_KEY}`
+    );
+    const movies = await response.json();
+    return movies;
+  };
 
-    const fetchMovies = async () => {
-        const response = await fetch('https://api.themoviedb.org/3/trending/all/' +
-            TRENDING_TIME + '?api_key=' + API_KEY);
-        const movies = await response.json();
-        return movies;
-    };
-
-    fetchMovies()
-        .then(movies => {
-            // console.log('f',movies.results);
-            localStorage.setItem('currentPageMuvie', JSON.stringify(movies.results));
+  fetchTrending()
+    .then(movies => {
+      Render(movies.results);
+      localStorage.setItem(
+        'currentPage',
+        JSON.stringify({
+          type: 'trending',
+          result: movies,
         })
-        .catch(error => console.log(error));
-
-
-
-    Render(localStorage.getItem('currentPageMuvie'));
-
-}
-
-
-function Render(muvies) {
-    let arrayMuvies = JSON.parse(muvies);
-    console.log('Render', arrayMuvies);
-    let resultHtml = [];
-    searchBtn.insertAdjacentHTML("afterend", '');
-    arrayMuvies.forEach(muvie => {
-        resultHtml.push('<li>Title ' + muvie.title + '</li>');
-        // resultHtml.push('<img src="' + muvie.poster_path + '" alt="'+ muvie.title +'"></li>');
-        resultHtml.push('<li>genre_ids ' + muvie.genre_ids + '</li>');
-        resultHtml.push('<li>vote_average ' + muvie.vote_average + '</li>');
+      );
+    })
+    .catch(error => {
+      console.log(error);
     });
-    // console.log('Afrter  Render', resultHtml.join(''));
-    searchBtn.insertAdjacentHTML("afterend", resultHtml.join(''));
 }
 
-//https://api.themoviedb.org/3/trending/all/day?api_key=<<api_key>>
-
-// конкретний фільм
-// https://api.themoviedb.org/3/movie/550?api_key=50540b41e66ef631d8d57e13679f9024
+function Render(movies) {
+  markupContainer.innerHTML = movies
+    .map(movie => {
+      return `
+    <li>
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
+        movie.title
+      }">
+      <div>
+        <p class="card__title">${movie.title}</p>
+        <p class="card__genres">${movie.genre_ids}</p>
+        <p class="card__year">${parseInt(movie.release_date)}</p>
+      </div>
+    </li>`;
+    })
+    .join('');
+}
