feb8a24245c82e35ea356e65e260c07e92c9d01e
iragerj <iragerj@gmail.com>
2023-02-04 13:13:04 +0200

    added event listeners to modal

--- src/index.js
+++ src/index.js
@@ -1,20 +1,2 @@
-/*
-    @param 'watched' | 'queue' listType
-    @param { Film } film
-*/
-
-function addFilmToList(listType, film) {
-  const storageKey = listType;
-
-  const filmList = JSON.parse(localStorage.getItem(storageKey)) || [];
-
-  filmList.push(film);
-
-  localStorage.setItem(storageKey, JSON.stringify(filmList));
-}
-
-// addFilmToList('watched', { name: 'Titanic' });
-// addFilmToList('queue', { name: 'Clon' });
-import './js/andrew_screept';
 import './js/trends-and-search';
 import './js/modal';
